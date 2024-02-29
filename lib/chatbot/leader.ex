e0f1191c565a94592d38a51ad1abf8e51fa97279
Cristian Gonzalez Trillo <cristian.gonzalez@nomasystems.com>
2024-02-29 10:57:21 +0100

    fix: apply PR suggestions

--- lib/chatbot/leader.ex
+++ lib/chatbot/leader.ex
@@ -42,31 +42,31 @@ defmodule Chatbot.Leader do
     state =
       key
       |> Telegram.Api.request("getUpdates", offset: last_seen + 1, timeout: 30)
-      |> handle_get_updates(state)
+      |> do_handle_get_updates(state)
     # Re-trigger the looping behavior
     next_loop()
     {:noreply, state}
   end
 
-  defp handle_get_updates({:ok, []}, state) do
+  defp do_handle_get_updates({:ok, []}, state) do
     state
   end
 
-  defp handle_get_updates({:ok, updates}, state) do
-    %{last_seen: last_seen, workers_data: updated_workers_data} = handle_multiple_updates(updates, state.last_seen, state.bot_key, state.workers_data)
+  defp do_handle_get_updates({:ok, updates}, state) do
+    %{last_seen: last_seen, workers_data: updated_workers_data} = do_handle_multiple_updates(updates, state.last_seen, state.bot_key, state.workers_data)
     # Update the last_seen and the workers_data state
     %{state | last_seen: last_seen, workers_data: updated_workers_data}
   end
 
   # Handles updates from Telegram
-  defp handle_multiple_updates(updates, last_seen, key, workers_data) do
+  defp do_handle_multiple_updates(updates, last_seen, key, workers_data) do
     {max_update_id, updated_workers_data} = updates
     # Process our updates
     |> Enum.reduce(
       {last_seen, workers_data},
       fn update, {_, wd} ->
         Logger.info("Update received: #{inspect(update)}")
-        wd = handle_one_update(update, key, wd)
+        wd = do_handle_one_update(update, key, wd)
         {update["update_id"], wd}
       end
     )
@@ -75,7 +75,7 @@ defmodule Chatbot.Leader do
   end
 
   # Handles regular message updates
-  defp handle_one_update(%{"message" => msg, "update_id" => _}, _, workers_data) do
+  defp do_handle_one_update(%{"message" => msg, "update_id" => _}, _, workers_data) do
     stored_worker = Enum.find(workers_data, fn %{user_id: user_id} -> user_id == msg["chat"]["id"] end)
     # If there is already a process handling the conversation
     if stored_worker != nil do

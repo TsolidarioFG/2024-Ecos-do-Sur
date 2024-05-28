b31aa8e6335f07269e2587e346f90bd2c189c5d6
Cristian Gonzalez Trillo <cristian.gonzalez@nomasystems.com>
2024-05-28 12:14:57 +0200

    fix: apply PR suggestions

--- test/urls_test.exs
+++ test/urls_test.exs
@@ -7,8 +7,15 @@ defmodule UrlsTest do
   @regex ~r/(http|https):\/\/[^\s\"\'\]\)]+/
 
   test "check URLs" do
-    extract_urls(File.read!(@po_file_path))
-    |> Enum.each(&validate_and_check_url/1)
+    File.read!(@po_file_path)
+    |> extract_urls()
+    |> Task.async_stream(&validate_and_check_url/1, max_concurrency: 10, timeout: 10_000)
+    |> Enum.each(fn
+      {:ok, true} -> :ok
+      {:ok, false} -> Assertions.flunk("Some URLs failed the check.")
+      {:exit, _} -> Assertions.flunk("Task exited unexpectedly.")
+      {:error, reason} -> Assertions.flunk("Task failed: #{reason}")
+    end)
   end
 
   defp extract_urls(content) do
@@ -20,32 +27,26 @@ defmodule UrlsTest do
   defp validate_and_check_url(url) do
     case URI.parse(url) do
       %URI{scheme: scheme, host: host} when scheme in ["http", "https"] and not is_nil(host) ->
-        try do
-          check_url(url)
-        rescue
-          _ in Protocol.UndefinedError -> :ok
-          message -> Assertions.flunk("Error with URL #{url}: #{message.message}") # Ignore any certificate or unexpected errors.
-        end
+        check_url(url)
       _ ->
         Logger.warning("Invalid URL found: #{url}")
+        true
     end
   end
 
   defp check_url(url) do
     case HTTPoison.get(url, [], recv_timeout: 5000) do
       {:ok, %HTTPoison.Response{status_code: code}} when code in 200..399 ->
-        :ok
+        true
       {:ok, %HTTPoison.Response{status_code: code}} ->
-        log_and_fail(url, "Returned status code: #{code}")
+        Logger.error("Error with URL #{url}: Returned status code: #{code}")
+        false
       {:error, %HTTPoison.Error{reason: :timeout}} ->
         Logger.warning("Timeout for URL #{url}")
+        false
       {:error, %HTTPoison.Error{reason: reason}} ->
-        log_and_fail(url, "Failed to fetch URL: #{reason}")
+        Logger.error("Error with URL #{url}: Failed to fetch URL: #{reason}")
+        false
     end
-  end
-
-  defp log_and_fail(url, message) do
-    Logger.error("Error with URL #{url}: #{message}")
-    raise message
   end
 end

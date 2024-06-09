2ea9fb9e9294368818f34fd2884c80e241288689
Cristian <89994769+cristiangontri@users.noreply.github.com>
2024-06-09 13:04:15 +0200

    Improved history and fixed some issues (#28)

--- lib/chatbot/graphs/util/history_formatting.ex
+++ lib/chatbot/graphs/util/history_formatting.ex
@@ -1,30 +1,50 @@
 defmodule Chatbot.HistoryFormatting do
-
+  import ChatBot.Gettext
   def buildMessage(message, nil), do: "*" <> message <> "*"
   def buildMessage(message, history) do
     string_list =
-    Enum.map(history, fn state -> "✅ " <> state_to_string(state) end)
-    |> Enum.reverse()
+      Enum.map(history, &state_to_string/1)
+      |> Enum.filter(&(&1 != nil))
+      |> Enum.map(&("✅ " <> &1))
+      |> Enum.reverse()
 
     Enum.join(string_list, "\n") <> "\n\n" <> "*" <> message <> "*"
   end
 
   defp state_to_string(state) do
     case state do
-      {:start, :initial} -> "START"
-      {:U1 , :initial} -> "Urgente"
-      {:U1_1, :initial} -> "Yo"
-      {:U1_2, :initial} -> "Otro"
-      {:I1, :initial} -> "Información"
-      {:U2, :initial} -> "Sin peligro"
-      {:U2_1, :initial} -> "Lugar definido"
-      {:U2_3, :initial} -> "Con el personal del establecimiento"
-      {:U2_4, :initial} -> "Con otra persona."
-      {:start, :leisure} -> "Ocio"
-      {:EN, :leisure} -> "Niegan Entrada"
-      {:EN_1, :leisure} -> "Con hoja de reclamaciones"
-      {:EN_2, :leisure} -> "Necesito ayuda para rellenarla"
-      _ -> "Not Implemented Yet"
+      # INITIAL STATES  -----
+      {:U1_new, :initial} -> gettext("URGENT")
+      {:U2, :initial} -> gettext("NO DANGER")
+      # LEISURE STATES  -----
+      {:start, :leisure} -> gettext("LEISURE")
+      # HOSPITAL STATES -----
+      {:start, :hospital} -> gettext("HOSPITAL")
+      {:U1, :hospital} -> gettext("HOSPITAL SERVICE DENIAL")
+      # HOME STATES     -----
+      {:start, :home} -> gettext("HOME")
+      {:Q2, :home} -> gettext("TENANT")
+      {:Q3, :home}  -> gettext("LANDLORD")
+      {:Q3_1, :home}  -> gettext("WANT TO RENT")
+      # PER-WORK STATES -----
+      {:start, :work_per} -> gettext("PERSON AT WORK")
+      # SCHOOL STATES   -----
+      {:start, :school} -> gettext("SCHOOL")
+      {:TE_Q7, :school} -> gettext("TEACHER")
+      {:TE, :school} -> gettext("STUDENT")
+      {:AC, :school} -> gettext("ACTIVITIES")
+      {:AC_SEC, :school} -> gettext("SECRETARY")
+      {:AC_EX, :school} -> gettext("EXTRACURRICULAR")
+      {:AC_CA, :school} -> gettext("CANTEEN")
+      # COMMERCE STATES -----
+      {:start, :commerce} -> gettext("COMMERCE")
+      # WORK STATES     -----
+      {:start, :work} -> gettext("WORK")
+      {:EM, :work} -> gettext("EMPLOYEE")
+      {:Q3, :work} -> gettext("JOB CANDIDATE")
+      {:Q3_1, :work} -> gettext("WORKER")
+      {:Q3_2, :work} -> gettext("BOSS")
+      _ -> nil
     end
   end
 end

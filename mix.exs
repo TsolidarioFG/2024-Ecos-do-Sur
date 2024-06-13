9c7b97ecc051795066c82fe1dd5eccde1ead5b36
Cristian Gonzalez Trillo <cristian.gonzalez@nomasystems.com>
2024-06-13 13:25:27 +0200

    translate: to english

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Chatbot.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :observer, :wx, :runtime_tools],
       mod: {Chatbot.Application, []}
     ]
   end
